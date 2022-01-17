683ab2cea26d0189365dff16e87fff368acaae21
sanjraj8 <88064534+sanjraj8@users.noreply.github.com>
2022-01-17 16:29:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase;
+var playerArcher;
+
+
+function preload() {
+  backgroundImg = loadImage("assets/background.png");
+  baseimage = loadImage("assets/base.png");
+  playerimage = loadImage("assets/player.png");
+  playerArcher = loadImage("assets/playerArcher.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
+
+  //creating the object for PlayerArcher
+  playerArcher = new PlayerArcher(335,250,100,100);
+}
+
+function draw() {
+  background(backgroundImg);
+
+  Engine.update(engine);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+
+  
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+  playerArcher.display();
+}
+
+
+
+
